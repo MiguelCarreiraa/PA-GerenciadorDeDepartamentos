fd7ee75787bd004eb406f40882293689feefbc29
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-20 11:29:56 -0300

    Model

---
+++ src/main/java/com/Carreira/PA_GerenciadorDeDepartamentos/PaGerenciadorDeDepartamentosApplication.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDeDepartamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaGerenciadorDeDepartamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaGerenciadorDeDepartamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Carreira/PA_GerenciadorDeDepartamentos/PaGerenciadorDeDepartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDeDepartamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaGerenciadorDeDepartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
